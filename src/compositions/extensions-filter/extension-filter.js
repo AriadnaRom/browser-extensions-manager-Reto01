908a50f254b6c064edcb69f925a91cbb288d08ef
Romina Ariadna Romero Rivas <romina.romero@entelgy.com>
2026-08-24 03:20:08 -0500

    se realizaron cambios de estilo

--- src/compositions/extensions-filter/extension-filter.js
+++ src/compositions/extensions-filter/extension-filter.js
@@ -47,6 +47,15 @@ export class ExtensionFilter extends LitElement {
 
   _handleFilter(filter) {
     this.selectedFilter = filter;
+//cuando presiones un boten este  debe lanzar un evento 
+ this.dispatchEvent(
+    new CustomEvent("filter-change", {
+      detail: filter,
+      bubbles: true,
+      composed: true,
+    })
+  );
+
   }
 }
 
--- src/compositions/extentions-list/sectionHeader/section-header.js
+++ src/compositions/extentions-list/sectionHeader/section-header.js
@@ -1,8 +1,12 @@
-import { LitElement, html} from "lit";
+import { LitElement, html, css, unsafeCSS } from "lit";
 import "../../../components/type-text/type-text.js";
+import styles from "./section-header.scss?inline";
 import "../../../compositions/extensions-filter/extension-filter.js"
 
 export class SectionHeader extends LitElement {
+   static styles = css`
+      ${unsafeCSS(styles)}
+    `;
   static properties = {
     title: { type: String },
     align: { type: String },
@@ -16,20 +20,18 @@ export class SectionHeader extends LitElement {
 
   _renderContent() {
     return html`
-      <div>
-        <type-text
-          size="xl"
-          weight="bold"
-          .text="${this.title}"
-          .alignText="${this.align}"
-        ></type-text>
+      <div class="section-header">
+      <type-text
+        size="xl"
+        weight="bold"
+        .text=${this.title}
+        .alignText=${this.align}
+      ></type-text>
 
-<div class =filter> 
-  <extension-filter></extension-filter>
-
-</div>
-       
-  </div>
+      <div class="filter">
+        <extension-filter></extension-filter>
+      </div>
+    </div>
     `;
   }
 
