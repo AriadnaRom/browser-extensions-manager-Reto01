577b91fd67b381f2b12987e27bff7ae380ece5bb
Romina Ariadna Romero Rivas <romina.romero@entelgy.com>
2026-09-02 19:00:35 -0500

    Mover data.json a public

--- src/service/extension-service.js
+++ src/service/extension-service.js
@@ -1,4 +1,4 @@
-const EXTENSIONS_URL = "./data.json";
+const EXTENSIONS_URL = `${import.meta.env.BASE_URL}data.json`;
 //Ir a buscar los datos y devolverlos
 export async function getExtensions() {
   const response = await fetch(EXTENSIONS_URL);
